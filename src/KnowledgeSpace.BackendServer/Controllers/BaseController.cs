57988b225cc37763cdda3e2370de02e6ee48c254
nhanale <nhanale@kms-technology.com>
2021-04-12 09:43:29 +0700

    add api's version

--- src/KnowledgeSpace.BackendServer/Controllers/BaseController.cs
+++ src/KnowledgeSpace.BackendServer/Controllers/BaseController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace KnowledgeSpace.BackendServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     [Authorize("Bearer")]
     public class BaseController : ControllerBase
